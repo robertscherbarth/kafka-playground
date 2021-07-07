e2288694d1cbf0c873460cefbd9797a0624a19c5
Robert Scherbarth <r.scherbarth@icloud.com>
2021-07-07 21:12:33 +0200

    refactor consumer and add error listing

--- consumer/main.go
+++ consumer/main.go
@@ -9,11 +9,17 @@ import (
 var brokers = []string{"localhost:9092"}
 
 func main() {
-	consumer, err := sarama.NewConsumer(brokers, nil)
+	config := sarama.NewConfig()
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest //get offset for the oldest message on the topic
+	config.Consumer.Offsets.Retry.Max = 3
+	config.Consumer.Return.Errors = true
+
+	consumer, err := sarama.NewConsumer(brokers, config)
 	if err != nil {
 		log.Fatal("Could not create consumer: ", err.Error())
 	}
 
+	log.Println("start consuming")
 	subscribe("test", consumer)
 
 	// just for blocking the function
@@ -26,16 +32,22 @@ func subscribe(topic string, consumer sarama.Consumer) {
 	if err != nil {
 		log.Fatal("Error retrieving partitionList ", err.Error())
 	}
-	initialOffset := sarama.OffsetOldest //get offset for the oldest message on the topic
-
 	for _, partition := range partitionList {
-		pc, _ := consumer.ConsumePartition(topic, partition, initialOffset)
-
-		go func(pc sarama.PartitionConsumer) {
-			for message := range pc.Messages() {
-				// here it can be stored in a db or stuff
-				log.Printf("received message %v\n", string(message.Value))
+		pc, err := consumer.ConsumePartition(topic, partition, sarama.OffsetOldest)
+		go func() {
+			if err != nil {
+				log.Printf("could not create partition consumer, err: %s", err.Error())
 			}
-		}(pc)
+
+			for {
+				select {
+				case err := <-pc.Errors():
+					log.Printf("could not process message, err: %s", err.Error())
+				case message := <-pc.Messages():
+					// here it can be stored in a db or stuff
+					log.Printf("received message %v\n", string(message.Value))
+				}
+			}
+		}()
 	}
 }
